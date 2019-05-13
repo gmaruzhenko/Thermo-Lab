c6dc1f34acb34374259858d6229a18759c620104
gmar7777 <gmaruzhenko@yahoo.ca>
2019-05-13 14:24:05 -0700

    cleanup

--- interpereter.py
+++ interpereter.py
@@ -2,6 +2,9 @@
 # info source = https://arduino.stackexchange.com/questions/55941/serial-communication-between-python-and-arduino
 import serial
 import time  # Required to use delay functions
+import sys
+
+#Read from Arduino
 
 arduinoSerialData = serial.Serial('com8', 9600)  # Create Serial port object called arduinoSerialData
 time.sleep(10)  # wait for 2 secounds for the communication to get established
@@ -9,8 +12,8 @@ time.sleep(10)  # wait for 2 secounds for the communication to get established
 print arduinoSerialData.readline()  # read the serial data and print it as line
 print ("Enter 1 to turn ON LED and 0 to turn OFF LED")
 
-import sys
 
+#write to csv
 
 """
 Initializes a file for writing and returns the file name
