cebbb360cd9725bc0c52f838662f0bedba51d18c
gmar7777 <gmaruzhenko@yahoo.ca>
2019-05-15 15:31:00 -0700

    updated for opperation with pi and multiple writes

--- interpereter.py
+++ interpereter.py
@@ -6,13 +6,20 @@ import csv
 #Read from Arduino
 
 ser = serial.Serial(
-   port = 'COM4',
+   port = '../../../dev/ttyACM0',
    baudrate = 9600,
    timeout = 10
 )
-with open(datetime.utcnow().strftime("%Y_%m_%d_%H_%M_%S.csv"), "a", newline='') as csv_file:
-   writer = csv.writer(csv_file, delimiter=',')
-   while True:
-      data = ser.readline().decode().strip().split(',')
-      print (data)
-      writer.writerow(data)
+filename = datetime.utcnow().strftime("%Y_%m_%d_%H_%M_%S.csv")
+while True:
+   with open(str(filename), "a", newline='') as csv_file:
+      writer = csv.writer(csv_file, delimiter=',')
+      count = 0
+      while count < 1000:
+         data = ser.readline().decode().strip().split(',')
+         print (data)
+         writer.writerow(data)
+         count += 1
+   
+
+   
