4366031407ff847ee28519e276aa296cb45e22c2
Jiayi Tang <jiayitang10@gmail.com>
2019-05-17 16:39:20 -0700

    Added 3d plot

--- plotter.py
+++ plotter.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm as colormap
+from mpl_toolkits.mplot3d import Axes3D as plt3d
 import csv
 
 SENSOR_SPACING = 71.2 # mm
@@ -10,15 +12,33 @@ def tempToVoltage(t):
 def voltageToTemp(x):
     return 100*(x-0.75) + 25 
 
-data = np.loadtxt('RunMay17-1.csv', delimiter=',')
+data = np.loadtxt('RunMay17-2.csv', delimiter=',')
 time = data[:, 0] / 1000
-t = np.array([voltageToTemp(data[:, i] * 5 / 1024) for i in range(1,6)])
+temp = np.array([voltageToTemp(data[:, i] * 5 / 1024) for i in range(1,6)])
+x = np.array([i * SENSOR_SPACING for i in range(5)])
 
-plt.figure(1)
-plt.xlabel("Time (s)")
-plt.ylabel("Temperature (degC)")
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(111, projection = '3d')
 
-for i in range(len(t)):
-    plt.plot(time, t[i])
+ax2.set_xlabel("Position along rod (mm)")
+ax2.set_ylabel("Time (s)")
+ax2.set_zlabel("Temperature (degC)")
 
+time, x = np.meshgrid(time, x)
+
+print(x.shape)
+print(time.shape)
+
+print(temp.shape)
+
+ax2.plot_surface(x, time, temp, cmap = colormap.viridis)
 plt.show()
+
+# plt.figure(1)
+# plt.xlabel("Time (s)")
+# plt.ylabel("Temperature (degC)")
+
+# for i in range(len(t)):
+#     plt.plot(time, t[i])
+
+# plt.show()
